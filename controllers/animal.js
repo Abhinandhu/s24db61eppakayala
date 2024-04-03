99bd245c7d68d3c469cc72af7e602d67632c67a2
Abhinandhu <36483329+Abhinandhu@users.noreply.github.com>
2024-04-03 14:07:18 -0500

    sc8

--- controllers/animal.js
+++ controllers/animal.js
@@ -50,3 +50,25 @@ exports.animal_list = async function(req, res) {
             res.send(`{"error": ${err}}`);
             } 
 };
+
+
+exports.animal_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new animal();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"animal_type":"goat", "cost":12, "size":"large"}
+    document.name = req.body.name;
+    document.type = req.body.type;
+    document.age = req.body.age;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
+    
