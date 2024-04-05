1a65bf9230659733dacfea14c39392b688ebf64c
Abhinandhu <36483329+Abhinandhu@users.noreply.github.com>
2024-04-05 10:42:31 -0500

    lab12 done

--- controllers/animal.js
+++ controllers/animal.js
@@ -2,57 +2,58 @@
 var animal = require('../models/animal');
 
 // Controller function to list all animals
-exports.animal_list = function(req, res) {
+exports.animal_list = function (req, res) {
     res.send('NOT IMPLEMENTED: animal list');
 };
 
 // Controller function to get details of a specific animal
-exports.animal_detail = function(req, res) {
+exports.animal_detail = function (req, res) {
     res.send('NOT IMPLEMENTED: animal detail: ' + req.params.id);
 };
 
 // Controller function to create a new animal
-exports.animal_create_post = function(req, res) {
+exports.animal_create_post = function (req, res) {
     res.send('NOT IMPLEMENTED: animal create POST');
 };
 
 // Controller function to delete a specific animal
-exports.animal_delete = function(req, res) {
+exports.animal_delete = function (req, res) {
     res.send('NOT IMPLEMENTED: animal delete DELETE ' + req.params.id);
 };
 
 // Controller function to update details of a specific animal
-exports.animal_update_put = function(req, res) {
+exports.animal_update_put = function (req, res) {
     res.send('NOT IMPLEMENTED: animal update PUT ' + req.params.id);
 };
 
 
 // Animals List
-// exports.animal_list = async function(req, res) {
-//     try{
-//     theanimal = await animal.find();
-//     res.send(theanimal);
-//     }
-//     catch(err){
-//     res.status(500);
-//     res.send(`{"error": ${err}}`);
-//     } 
-//    };
-
 exports.animal_list = async function(req, res) {
+    try{
+    theanimal = await animal.find();
+    res.send(theanimal);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    } 
+   };
+
+exports.animal_view_all_Page = async function (req, res) {
 
     try {
-        const animals = await animal.find();
+        animals = await animal.find();
         res.render('animal', { title: 'Animal Search Results', results: animals });
-    } 
-    catch(err){
-            res.status(500);
-            res.send(`{"error": ${err}}`);
-            } 
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
 };
 
 
-exports.animal_create_post = async function(req, res) {
+// Handle animal create on POST.
+exports.animal_create_post = async function (req, res) {
     console.log(req.body)
     let document = new animal();
     // We are looking for a body, since POST does not have query parameters.
@@ -62,13 +63,44 @@ exports.animal_create_post = async function(req, res) {
     document.name = req.body.name;
     document.type = req.body.type;
     document.age = req.body.age;
-    try{
-    let result = await document.save();
-    res.send(result);
+    try {
+        let result = await document.save();
+        res.send(result);
     }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
     }
-    };
-    
+};
+
+
+// exports.animal_detail = async function(req, res) {
+//     console.log("detail" + req.params.id)
+//     try {
+//     result = await animal.findById( req.params.id)
+//     res.send(result)
+//     } catch (error) {
+//     res.status(500)
+//     res.send(`{"error": document for id ${req.params.id} not found`);
+//     }
+//    }
+
+
+exports.animal_update_put = async function (req, res) {
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await animal.findById(req.params.id)
+        // Do updates of properties
+        if (req.body.animal_type)
+            toUpdate.animal_type = req.body.animal_type;
+        if (req.body.animal_size) toUpdate.animal_size = req.body.animal_size;
+        if (req.body.animal_price) toUpdate.animal_price = req.body.animal_price;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id}
+        failed`);
+    }
+};
--- routes/animal.js
+++ routes/animal.js
@@ -3,11 +3,16 @@ var router = express.Router();
 const animal_controller = require('../controllers/animal');
 
 /* GET all animals */
-router.get('/', animal_controller.animal_list);
+router.get('/', animal_controller.animal_view_all_Page);
 
 /* GET home page. */
 // router.get('/', function(req, res, next) {
 //   res.render('animal', { title: 'Search Results for Class Animal' });
 // });
+router.put('/', function(req, res) {
+  if(req.body.checkboxsale)toUpdate.sale = true;
+  else toUpdate.same = false;
+  })
+  router.get('/', animal_controller.animal_update_put );
 
 module.exports = router;
